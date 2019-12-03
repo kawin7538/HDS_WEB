854ef90b4321532ea9d0d1c8446452095f529141
Kawin Chinpong <kawin7538@hotmail.com>
2019-12-03 14:39:37 +0700

    Edit for security

--- public/database.js
+++ public/database.js
@@ -1,13 +1,15 @@
 
+//Simply find from your firebase api
+
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyCNul7APvgbaFXOj4iJn7E70J4m4T7cLhM",
-  authDomain: "hds-web-back.firebaseapp.com",
-  databaseURL: "https://hds-web-back.firebaseio.com",
-  projectId: "hds-web-back",
+  apiKey: "Use Yours",
+  authDomain: "Use Yours",
+  databaseURL: "Use Yours",
+  projectId: "Use Yours",
   storageBucket: "",
-  messagingSenderId: "233562637716",
-  appId: "1:233562637716:web:f9d6edda3b1daec6"
+  messagingSenderId: "Use Yours",
+  appId: "Use Yours"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
